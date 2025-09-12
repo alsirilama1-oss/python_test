a43c706a425402c9137c2c384d7ec14b1242010e
alsirilama1-oss <alsirilama1@gmail.com>
2025-09-12 12:56:23 +0200

    Second changes

---
+++ main.py
@@ -0,0 +1,14 @@
+def første_avsnitt():
+   print("Her starter vår historie...")
+
+def andre_avsnitt():
+   pass # Blir laget senere
+
+def tredje_avsnitt():
+   pass # Blir laget senere
+
+def print_ut_historien():
+ første_avsnitt()
+ andre_avsnitt()
+ tredje_avsnitt()
+ print_ut_historien()
